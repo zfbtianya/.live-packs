a5df257c2d10534a859b52bb20926b7728878f45
zengfb <zengfb@majorbio_cluster.com>
2014-03-05 08:56:00 +0800

    add R-pack

---
+++ R-pack/config/init-ess.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path "/home/zengfb/emacs/live-packs/R-pack/lib/ESS/lisp/")
+(load "ess-site")
---
+++ R-pack/info.el
@@ -0,0 +1,3 @@
+(live-pack-name "ess")
+(live-pack-version "0.0.1")
+(live-pack-description "ess")
